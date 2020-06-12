152132e5d2b4fadec57a21c419d0482553680e9b
unknown <smalar1998@gmail.com>
2020-06-12 12:04:15 +0530

    pattern program is commited

--- pattern_program.py
+++ pattern_program.py
@@ -1,20 +1,23 @@
 #to create a pattern program
 import math
 def pattern(n):
-    div=math.ceil((n/2))
-    div1=n-div
-    k=1
-    for i in range(div):
-        for j in range(0,k):
+    if n%2==0:
+        print("enter valid odd number")
+    else:
+        div=math.ceil((n/2))
+        div1=n-div
+        k=1
+        for i in range(div):
+            for j in range(0,k):
+                    print("*",end=" ")
+            k=k+2
+            print()
+        l=n-2
+        for i in range(div1):
+            for j in range(0,l):
                 print("*",end=" ")
-        k=k+2
-        print()
-    l=n-2
-    for i in range(div1):
-        for j in range(0,l):
-            print("*",end=" ")
-        l=l-2
-        print()
+            l=l-2
+            print()
 print("enter the rows")
 n=int(input())
 pattern(n)
