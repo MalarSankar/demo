aeb3de10d1fc51dba2e401ded60a8b4e0a028925
MalarSankar <smalar1998@gmail.com>
2020-07-09 17:24:41 -0700

    module assignment is commited

---
+++ module_file1.py
@@ -0,0 +1,4 @@
+x=10
+y=10
+def func(self):
+    return "Hi,This is "+self
---
+++ module_file2.py
@@ -0,0 +1,4 @@
+import module_file1
+print(module_file1.func("Malar"))
+print("x:",module_file1.x)
+print("y:",module_file1.y)
